203648b6e579d9046a22f332fb0672f6347fecc5
kleva-j <kasmickleva@gmail.com>
2024-09-03 13:20:43 +0100

    feat: setup next-themes with theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
+import { type PropsWithChildren } from "react";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "styles/tailwind.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
