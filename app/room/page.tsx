c1ff365cc713e661258423e8dbcb2a496794da83
kleva-j <kasmickleva@gmail.com>
2024-09-20 13:07:48 +0100

    feat: preload available rooms in the room-list component

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -1,18 +1,26 @@
-import { RoomCard } from "@/components/room-card";
+import { api } from "@/convex/_generated/api";
+import { getAuthToken } from "@/lib/auth";
+import { RoomList } from "@/room/room-list";
+import { preloadQuery } from "convex/nextjs";
 
 export default async function RoomsPage() {
-  return (
-    <div>
-      <h2 className="mx-auto w-max text-2xl">Welcome to the rooms page</h2>
-      <div className="mt-16 grid place-items-center">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          <RoomCard />
-          <RoomCard />
-          <RoomCard />
-          <RoomCard />
-          <RoomCard />
+  try {
+    const token = await getAuthToken();
+    const query = await preloadQuery(api.rooms.getManyByUser, {}, { token });
+    return (
+      <div>
+        <h2 className="mx-auto w-max text-2xl">Welcome to the rooms page</h2>
+        <div className="mt-16 grid place-items-center">
+          <RoomList query={query} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-2xl">Error</h2>
+        <p className="text-lg">There was an error fetching rooms</p>
+      </div>
+    );
+  }
 }
---
+++ app/room/room-list.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { RoomCard } from "@/components/room-card";
+import { api } from "@/convex/_generated/api";
+
+import { Preloaded, usePreloadedQuery } from "convex/react";
+
+type RoomListProps = {
+  query: Preloaded<typeof api.rooms.getManyByUser>;
+};
+
+export const RoomList = ({ query }: RoomListProps) => {
+  const rooms = usePreloadedQuery(query);
+
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {Array.isArray(rooms)
+        ? rooms.map((room) => <RoomCard key={room._id} room={room} />)
+        : null}
+    </div>
+  );
+};
