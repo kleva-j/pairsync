985360ee3785e68176d80aee4da1454d53451218
kleva-j <kasmickleva@gmail.com>
2024-09-23 14:12:29 +0100

    fix: posthog provider initialization error

--- components/providers/posthog-provider.tsx
+++ components/providers/posthog-provider.tsx
@@ -4,19 +4,19 @@ import posthog from "posthog-js";
 
 import { PostHogProvider } from "posthog-js/react";
 
-import type { PropsWithChildren } from "react";
+import { type PropsWithChildren, useEffect } from "react";
 
 import { env } from "env.mjs";
 
-if (typeof window !== "undefined") {
-  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only",
-    capture_pageview: false,
-    capture_pageleave: true,
-  });
-}
-
 export function PHProvider({ children }: PropsWithChildren) {
+  useEffect(() => {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: "identified_only",
+      capture_pageview: false,
+      capture_pageleave: true,
+    });
+  }, []);
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
